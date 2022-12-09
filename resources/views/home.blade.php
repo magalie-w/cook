fef139dddb0d8022da3f268810bb0c1832ed9586
magalie <magalie.waligora@gmail.com>
2022-12-09 19:41:58 +0100

    route + view home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.base')
+
+@section('title')
+    Accueil - @parent
+@endsection
+
+@section('content')
+    <div class="mx-auto w-[900px] flex flex-wrap justify-between">
+        @foreach ($recettes as $recette)
+            <div class="mt-[50px] text-center bg-amber-900 rounded text-white">
+                <img src="{{ $recette->img }}" alt="image de la recette" width="200" class="rounded">
+
+                <h1 class="py-3">{{ $recette->titre }}</h1>
+            </div>
+        @endforeach
+    </div>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>
+        @section('title') {{ config('app.name') }} @show
+    </title>
+</head>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+<body>
+    <div class="mx-auto w-[900px]">
+        <img src="logo.png" alt="logo" width="200">
+        <div class="w-[900px] h-[10px] rounded bg-amber-900"></div>
+    </div>
+
+    @yield('content')
+</body>
+</html>
