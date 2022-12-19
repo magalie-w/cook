8fb1dfe3d21b910756f2c0f2181e3c1870e86b58
magalie <magalie.waligora@gmail.com>
2022-12-19 20:25:30 +0100

    middleware

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,12 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/recette', [RecetteController::class, 'index'])->name('recette.index');
 Route::get('/recette/{titre}', [RecetteController::class, 'show'])->name('recette.show');
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
+// ->middleware('guest') si on est connecté ça redirige vers /dashboard
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'store'])->name('store');
-Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
+// Deconnexion et rédirige vers /login
+Route::get('/logout', [LoginController::class, 'destroy'])->name('logout')->middleware('auth');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+// ->middleware('auth') si on est pas connecté on n'a pas accès au dashboard et redirige vers /login
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
